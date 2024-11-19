d684e2c3e6a14efd5cf096323938845da526822f
Emily Ng <ngem3@oocl.com>
2024-11-19 17:36:09 +0800

    refactor: refactor MarsRover, add constructor, refactor showStatus

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -1,5 +1,15 @@
 package org.example;public class MarsRover {
+    private int x;
+    private int y;
+    private char direction;
+
+    public MarsRover() {
+        this.x = 0;
+        this.y = 0;
+        this.direction = 'N';
+    }
+
     public String showStatus() {
-        return "0:0:N";
+        return x + ":" + y + ":" + direction;
     }
 }
