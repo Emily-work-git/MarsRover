eedecd895d53a9acf8ccb1660dc702d8329ac2ed
Emily Ng <ngem3@oocl.com>
2024-11-19 17:44:54 +0800

    test: add test case for setDirection (setDirection: E/S/W)

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -11,12 +11,37 @@ public class MarsRoverTest {
         String expectedStatus = "0:0:N";
         assertEquals(expectedStatus, actualStatus);
     }
+//    Test setDirection
     @Test
     public void should_face_north_when_setDirection_to_north(){
         MarsRover rover = new MarsRover();
         rover.setDirection('N');
         String actualStatus = rover.showStatus();
         String expectedStatus = "0:0:N";
+        assertEquals(expectedStatus, actualStatus);
+    }
+    @Test
+    public void should_face_east_when_setDirection_to_east(){
+        MarsRover rover = new MarsRover();
+        rover.setDirection('E');
+        String actualStatus = rover.showStatus();
+        String expectedStatus = "0:0:E";
+        assertEquals(expectedStatus, actualStatus);
+    }
+    @Test
+    public void should_face_south_when_setDirection_to_south(){
+        MarsRover rover = new MarsRover();
+        rover.setDirection('S');
+        String actualStatus = rover.showStatus();
+        String expectedStatus = "0:0:S";
+        assertEquals(expectedStatus, actualStatus);
+    }
+    @Test
+    public void should_face_west_when_setDirection_to_west(){
+        MarsRover rover = new MarsRover();
+        rover.setDirection('W');
+        String actualStatus = rover.showStatus();
+        String expectedStatus = "0:0:W";
         assertEquals(expectedStatus, actualStatus);
     }
 }
