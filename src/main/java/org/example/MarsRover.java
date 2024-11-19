315f26d8a278309420a2f23da3d1943fee8a1213
Emily Ng <ngem3@oocl.com>
2024-11-19 17:50:33 +0800

    feat: add executeCommand method - turn left

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -15,4 +15,17 @@ package org.example;public class MarsRover {
     public void setDirection(char newDirection){
         this.direction = newDirection;
     }
+    public String executeCommand(String command) {
+        char[] directions = {'N', 'E', 'S', 'W'};
+        for (int i = 0; i < directions.length; i++) {
+            if (directions[i] == direction) {
+                if (command.equals("L")) {
+                    int newDirectionIndex = i==0? directions.length - 1: i-1;
+                    setDirection(directions[newDirectionIndex]);
+                }
+                break;
+            }
+        };
+        return showStatus();
+    }
 }
