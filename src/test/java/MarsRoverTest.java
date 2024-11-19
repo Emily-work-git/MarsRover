aae0f45894946dc0b9f8b37a569201507e51c383
Emily Ng <ngem3@oocl.com>
2024-11-19 17:41:54 +0800

    test: add test case for setDirection (setDirection to north)

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -11,4 +11,12 @@ public class MarsRoverTest {
         String expectedStatus = "0:0:N";
         assertEquals(expectedStatus, actualStatus);
     }
+    @Test
+    public void should_face_north_when_setDirection_to_north(){
+        MarsRover rover = new MarsRover();
+        rover.setDirection('N');
+        String actualStatus = rover.showStatus();
+        String expectedStatus = "0:0:N";
+        assertEquals(expectedStatus, actualStatus);
+    }
 }
